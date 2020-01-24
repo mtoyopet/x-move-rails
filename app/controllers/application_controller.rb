65a37db00ccd74445af49e46c56b16c20ef692ea
Momoko Toyota <toyopet-momoko@hotmail.co.jp>
2020-01-24 12:41:01 +0900

    twitterログインができた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
-  protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
 
+  protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
+  before_action :skip_session
+
+  protected
+
+  def skip_session
+    request.session_options[:skip] = true
+  end
+
+  # def skip_session
+  #   unless current_user
+  #     request.session_options = {drop: true}
+  #   end
+  # end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ TWITTER_KEY = Rails.application.credentials[Rails.env.to_sym][:twitter][:twitter
 TWITTER_SECRET = Rails.application.credentials[Rails.env.to_sym][:twitter][:twitter_api_secret]
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV.fetch("TWITTER_KEY"), ENV.fetch("TWITTER_SECRET"), callback_url: "http://127.0.0.1/api/v1/auth/twitter/callback"
+  provider :twitter, TWITTER_KEY, TWITTER_SECRET, callback_url: "http://127.0.0.1:3000/api/v1/auth/twitter/callback"
 end
