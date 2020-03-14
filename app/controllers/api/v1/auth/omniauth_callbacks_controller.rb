a8c4c10deb27948be1694aa000a04afe88df7075
松田信介 <>
2020-03-15 02:51:06 +0900

    修正

--- app/controllers/api/v1/auth/omniauth_callbacks_controller.rb
+++ app/controllers/api/v1/auth/omniauth_callbacks_controller.rb
@@ -27,18 +27,18 @@ module Api
         end
 
         def render_data_or_redirect(message, data, user_data = {})
-          if Rails.env.production?
-            if ['inAppBrowser', 'newWindow'].include?(omniauth_window_type)
-              render_data(message, user_data.merge(data))
-            elsif auth_origin_url
-              redirect_to DeviseTokenAuth::Url.generate(auth_origin_url, data.merge(blank: true))
-            else
-              fallback_render data[:error] || 'An error occurred'
-            end
-          else
+          # if Rails.env.production?
+          #   if ['inAppBrowser', 'newWindow'].include?(omniauth_window_type)
+          #     render_data(message, user_data.merge(data))
+          #   elsif auth_origin_url
+          #     redirect_to DeviseTokenAuth::Url.generate(auth_origin_url, data.merge(blank: true))
+          #   else
+          #     fallback_render data[:error] || 'An error occurred'
+          #   end
+          # else
             #  // わかりやすい様に開発時はjsonとして結果を返す
             render json: @resource, status: :ok
-          end
+          # end
         end
 
           # // twitterから取得する絵文字を取り払うメソッドたち　DBエラーが起きるときにコメントイン
